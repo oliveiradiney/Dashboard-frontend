677800a97d788d27cb9110c4d00f61dc1407a061
oliveiradiney <dineyoliveira1999@gmail.com>
2022-01-06 01:21:46 -0300

    Criando Form Login

--- src/pages/Login/LoginComponent.vue
+++ src/pages/Login/LoginComponent.vue
@@ -6,6 +6,23 @@
                 <header>
                     <h1>Login do sistema</h1>
                 </header>
+
+                <div class="form">
+                    <form>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">E-mail</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                           
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Senha</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1">
+                        </div>
+                        <button type="submit" class="btn btn-primary" @click.prevent="login">Entrar</button>
+
+                    </form>
+                </div>
+
             </div>
         </div>
 
@@ -13,7 +30,17 @@
 </template>
 <script>
 export default {
+    name: 'LogingComponent',
+    data(){
+        return{
 
+        }
+    },
+    methods: {
+        login(){
+            this.$router.push({name: 'home'});
+        }
+    }
 }
 </script>
 

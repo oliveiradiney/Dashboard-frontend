81963161989736023f05fcbaad6e502d8846e43b
oliveiradiney <dineyoliveira1999@gmail.com>
2022-01-06 00:28:21 -0300

    Configurando vue-router, rotas login e home e instalando sass

---
+++ src/pages/Login/LoginComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="login">
+
+        <div class="info">
+            <h1 class="title-login">Titulo</h1>
+            <p>Login</p>
+        </div>
+
+        <div class="footer">
+            <h1>Rodapé</h1>
+        </div>
+      
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" src="./style.scss" scoped />

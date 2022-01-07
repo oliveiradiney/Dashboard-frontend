78715a7449732f17989c97db6f623e755b8190e6
oliveiradiney <dineyoliveira1999@gmail.com>
2022-01-06 23:37:56 -0300

    Criando oo componente SideBar

--- src/pages/Dashboard/components/SideBarComponent.vue
+++ src/pages/Dashboard/components/SideBarComponent.vue
@@ -1,6 +1,25 @@
 <template>
-    <div>
-        <p>Sidebar</p>
+    <div class="sidebar">
+        <div class="top">
+            <div class="avatar">
+                <span><i class="fa fa-user"></i></span>
+            </div>
+
+            <div class="info">
+                <p class="name">Diney Oliveira</p>
+                <p class="status">online</p>
+            </div>
+        </div>
+       
+        <div class="menu">
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -11,5 +30,48 @@ export default {
 </script>
 
 <style lang="scss">
+.sidebar{
 
+    .top{
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 25px 5px;
+        margin-top: 60px;
+    }
+
+    .avatar{
+        margin-right: 10px;
+
+        span{
+            display: block;
+            width: 50px;
+            height: 50px;
+            border-radius: 50%;
+            background-color:#CCC;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 25px;
+            color: #666666;
+        }
+    }
+
+    .info{
+
+        p{
+            margin: 0;
+            color: #fff;
+        }
+        .name{
+             font-size: 14px;
+        }
+        .status{
+           
+            font-size: 10px;
+        }
+    }
+
+}
 </style>

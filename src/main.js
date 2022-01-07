c0a6e115536cb97fbe6286c564820bc4cd122ece
oliveiradiney <dineyoliveira1999@gmail.com>
2022-01-06 22:05:48 -0300

    Finalizando o componente NavBar

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+
 
 import LoginComponent from './pages/Login/LoginComponent'
 import HomeComponent from './pages/Home/HomeComponent'
@@ -19,6 +22,7 @@ const router = new VueRouter({
   routes
 })
 
+/* eslint-disable no-new */
 new Vue({
   router,
   render: h => h(App),
--- src/pages/Dashboard/components/NavBarComponent.vue
+++ src/pages/Dashboard/components/NavBarComponent.vue
@@ -1,15 +1,35 @@
 <template>
-    <div>
-        <p>NavBar component</p>
+    <div class="navbar">
+        <h3 class="logo"><i class="fa fa-image"></i> Logo</h3>
+        <a href="#" @click.prevent="logout" class="logout">Sair <i class="fa fa-sign-out-alt"></i></a>
     </div>
 </template>
 
 <script>
 export default {
     name: 'NavBarComponent',
+    methods: {
+        logout(){
+            this.$router.push({name: 'login'})
+        }
+    }
 }
 </script>
 
 <style lang="scss">
+.navbar{
+
+    .logo{
+        color: #fff;
+    }
+
+    .logout{
+        color: #fff;
+    }
+    .logout{
+        color: #fff;
+        text-decoration: none;
+    }
+}
 
 </style>

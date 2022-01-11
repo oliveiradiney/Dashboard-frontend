5706b5783f062bc109898f7a2916f713628572cd
oliveiradiney <dineyoliveira1999@gmail.com>
2022-01-10 23:58:03 -0300

    Consumindo API Axios - Pegando informações de uma API REST com axios e vue js

--- src/pages/Home/HomeComponent.vue
+++ src/pages/Home/HomeComponent.vue
@@ -26,6 +26,13 @@
                 
             </div>
             
+            <div class="mt-5">
+                <div class="row">
+                    <div class="col-12 col-md-6">
+                        <ListsComponent />
+                    </div>
+                </div>
+            </div>
           
         </div>
     </DashboardComponent>
@@ -33,11 +40,14 @@
 <script>
 import DashboardComponent from '../Dashboard/DashboardComponent';
 import CardsComponent from '../../components/CardsComponent';
+import ListsComponent from '../../components/ListsComponent'
 
 export default {
+    name: 'HomeComponent',
     components:{
         DashboardComponent,
-        CardsComponent
+        CardsComponent,
+        ListsComponent
     }
 }
 </script>
